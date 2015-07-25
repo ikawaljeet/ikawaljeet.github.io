872a2a7077946774ceb86507044140b1dd09b477
kawaljeet kumar <kawal4tech@gmail.com>
2015-07-25 19:57:11 +0530

    Update led.php

--- led.php
+++ led.php
@@ -3,7 +3,7 @@ $onoroff = $_GET["state"]; // Declares the request from index.html as a variable
 $textfile = "LEDstate.txt"; // Declares the name and location of the .txt file
  
 $fileLocation = "$textfile";
-$fh = fopen("LEDstate.txt", 'w   ') or die("Something went wrong!"); // Opens up the .txt file for writing and replaces any previous content
+$fh = fopen($fileLocation, 'w   ') or die("Something went wrong!"); // Opens up the .txt file for writing and replaces any previous content
 $stringToWrite = "$onoroff"; // Write either 1 or 0 depending on request from index.html
 fwrite($fh, $stringToWrite); // Writes it to the .txt file
 fclose($fh); 
